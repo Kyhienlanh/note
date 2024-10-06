4c29b0efaef5b9e5980fcae17ebe322f7b6e21fa
3920038a3 <kyhienlanh@gmail.com>
2024-10-06 17:47:23 +0700

    update images

--- app/src/main/java/com/example/compose/MainActivity.kt
+++ app/src/main/java/com/example/compose/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.compose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -12,6 +13,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import com.example.compose.ui.theme.ComposeTheme
@@ -76,16 +78,24 @@ fun NoteApp() {
             topBar = {
                 @OptIn(ExperimentalMaterial3Api::class)
                 TopAppBar(
+                    modifier = Modifier.fillMaxWidth(),
                     title = { Text("Ghi chú", color = Color.White) },
                     actions = {
                         IconButton(onClick = { isDarkTheme = !isDarkTheme }) {
-                            Text(if (isDarkTheme) "Sáng" else "Tối", color = Color.White)
+                            // Hiển thị hình ảnh theo chế độ hiện tại
+                            val iconRes = if (isDarkTheme) R.drawable.ic_light_mode else R.drawable.ic_dark_mode
+                            Image(
+                                painter = painterResource(id = iconRes),
+                                contentDescription = if (isDarkTheme) "Chế độ sáng" else "Chế độ tối",
+                                modifier = Modifier.size(24.dp) // Kích thước hình ảnh
+                            )
                         }
                     },
                     colors = TopAppBarDefaults.topAppBarColors(
                         containerColor = MaterialTheme.colorScheme.primary
                     )
                 )
+
             },
             floatingActionButton = {
                 FloatingActionButton(onClick = { showDialog = true }) {
